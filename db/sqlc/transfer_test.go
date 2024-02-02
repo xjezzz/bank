05b227a76cd39e22f06cd553d4ff63f35f77cee2
ymukhame <qmerty1235@gmail.com>
2024-02-02 20:04:03 +0600

    part 8

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,67 @@
+package db
+
+import (
+	"context"
+	"github.com/MaksimDzhangirov/backendBankExample/db/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestCreateTransfer(t *testing.T) {
+	account1, account2 := createRandomAccount(t), createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
+}
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+
+	arg := CreateTransferParams{
+		account1.ID,
+		account2.ID,
+		util.RandomMoney(),
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+	require.Equal(t, transfer.Amount, arg.Amount)
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	require.NotZero(t, transfer.FromAccountID)
+	require.NotZero(t, transfer.ToAccountID)
+	return transfer
+}
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	transfer1 := createRandomTransfer(t, account1, account2)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	for i := 0; i < 5; i++ {
+		createRandomTransfer(t, account1, account2)
+		createRandomTransfer(t, account2, account1)
+	}
+	arg := ListTransfersParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account1.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
